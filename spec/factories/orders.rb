ed5a7999a6b036650fb36f9088ca39335ab1de8a
Aaron Norling <arn@nrlng.com>
2025-03-13 08:08:45 -0700

    better factory for orders
    breaks the raw data line into easier to read pieces and uses the data
    set elsewhere (in the columns) for the raw data field so that they all
    match

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -14,6 +14,21 @@ FactoryBot.define do
     payment_2_id { "7ac2a2b3-9b4d-442d-bbe7-d0327086db0e" }
     payment_2_amount { 11.71 }
 
-    raw_data { "order_id,ordered_at,item_type,price_per_item,quantity,shipping,tax_rate,payment_1_id,payment_1_amount,payment_2_id,payment_2_amount\n#{external_order_id},#{ordered_at},#{item_type},#{price_per_item},2,1.97,0.033,04efdd47-9e4f-4da4-a41e-21d619e3f560,12.32,7ac2a2b3-9b4d-442d-bbe7-d0327086db0e,11.71" }
+    raw_data do
+      headers = %w[
+        order_id ordered_at item_type price_per_item quantity shipping tax_rate
+        payment_1_id payment_1_amount payment_2_id payment_2_amount
+      ]
+
+      values = [
+        external_order_id, ordered_at, item_type, price_per_item, quantity,
+        shipping, tax_rate, payment_1_id, payment_1_amount, payment_2_id, payment_2_amount
+      ]
+
+      CSV.generate do |csv|
+        csv << headers
+        csv << values
+      end.strip
+    end
   end
 end
